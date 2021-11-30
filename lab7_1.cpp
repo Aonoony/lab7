a77c6f7ec15a55462a21ac630fac2064512adede
Nitchamon <Aonoony@gmail.com>
2021-11-30 18:12:35 +0700

    "lab7_1.cpp completed"

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,16 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string text, rev, a, b;
+    cout << "Input text: ";
+	cin>>text;
+	rev=func1(text);
+    cout << "Reversed text: "<<rev<<endl;
+	cout << "Palindrome: ";
+	if(func2(text)==func2(rev)){
+		cout<<"Yes";
+	}else{
+		cout<<"No";
+	}
     return 0;
 }
